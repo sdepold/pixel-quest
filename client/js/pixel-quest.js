e593bf5339f7d8293b81caf362daabbc2e96ca07
Sascha Depold <sascha@depold.com>
2013-08-25 20:17:21 +0200

    first contact with server includes identifier

--- client/js/pixel-quest.js
+++ client/js/pixel-quest.js
@@ -1,27 +1,62 @@
 window.PixelQuest = {
   initialize: function() {
+    var id = this.getIdentifier()
+
+    if (!id) {
+      id = this.generateIdentifier()
+      this.setIdentifier(id)
+    }
+
     // initialize the game
     this.game = new PixelQuest.Game()
 
     // exec the render loop
     setInterval(function() {
       this.game.render()
+
+      if (this.player && this.socket) {
+        var data = this.player.toJSON()
+        data.id = id
+        this.socket.emit('player#update', data)
+      }
+
     }.bind(this), 10)
 
     // connect to the server and load the user's data
-    window.PixelQuest.connectToServer(function(playerData) {
+    window.PixelQuest.connectToServer(id, function(playerData) {
       this.player = new PixelQuest.Player(playerData.x, playerData.y, playerData)
       this.game.renderables.push(this.player)
 
       var interaction = new PixelQuest.Interaction(this.player)
       interaction.bindKeyboardToPlayer()
-
     }.bind(this))
   },
 
-  connectToServer: function(callback) {
-    var socket = io.connect('http://localhost')
-    socket.on('player#joined', callback)
-    socket.emit('player#join', { id: 'foo' })
+  connectToServer: function(id, callback) {
+    this.socket = io.connect('http://localhost')
+    this.socket.on('player#joined', callback)
+    this.socket.emit('player#join', { id: id })
+  },
+
+  getIdentifier: function() {
+    var cookies = {}
+
+    ;(document.cookie || "").split(';').forEach(function(pair) {
+      var split = pair.split("=")
+      cookies[split[0].trim()] = split[1].trim()
+    })
+
+    return cookies.PixelQuestIdentifier
+  },
+
+  setIdentifier: function(id) {
+    document.cookie = 'PixelQuestIdentifier=' + id + '; expires=Tue, 1 Jan 2030 00:00:00 UTC; path=/'
+  },
+
+  generateIdentifier: function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    })
   }
 }
