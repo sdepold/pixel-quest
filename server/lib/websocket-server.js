47607eba567b417c8eaae6c3ee09919675411964
Sascha Depold <sascha@depold.com>
2013-08-25 20:17:59 +0200

    simplistic database approach

--- server/lib/websocket-server.js
+++ server/lib/websocket-server.js
@@ -3,6 +3,9 @@ var io = require('socket.io')
 var WebSocket = module.exports = function(server) {
   this.server = server
   this.io     = null
+  this.db     = {
+    players: {}
+  }
 }
 
 WebSocket.prototype.listen = function() {
@@ -16,7 +19,8 @@ WebSocket.prototype.listen = function() {
 
 WebSocket.prototype.observeEvents = function() {
   var events = {
-    'player#join': 'onPlayerJoin'
+    'player#join': 'onPlayerJoin',
+    'player#update': 'onPlayerUpdate'
   }
 
   Object.keys(events).forEach(function(eventName) {
@@ -25,5 +29,15 @@ WebSocket.prototype.observeEvents = function() {
 }
 
 WebSocket.prototype.onPlayerJoin = function(data) {
-  this.socket.emit('player#joined', { x: 50, y: 70, movementDelay: 50 })
+  if (!this.db.players[data.id]) {
+    this.db.players[data.id] = { x: 50, y: 70, movementDelay: 50 }
+  }
+
+  this.socket.emit('player#joined', this.db.players[data.id])
+}
+
+WebSocket.prototype.onPlayerUpdate = function(data) {
+  if (this.db.players[data.id]) {
+    this.db.players[data.id] = data
+  }
 }
