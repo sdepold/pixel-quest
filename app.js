69ff745d894f10c38da0e617b8b5895f76af2570
Sascha Depold <sascha@depold.com>
2013-08-28 07:44:27 +0200

    renamed server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var Server    = require('./server/lib/server.js')
+var HttpServer    = require('./server/lib/http-server.js')
   , WebSocket = require('./server/lib/websocket-server.js')
 
-var server = new Server()
+var server = new HttpServer()
   , socket = new WebSocket(server)
 
 socket.listen()
