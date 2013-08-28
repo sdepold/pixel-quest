f9f1fd955eb184b35a73e6c5fa27a21739baf53f
Sascha Depold <sascha@depold.com>
2013-08-28 21:00:04 +0200

    added active player identifier

--- client/js/pixel-quest.js
+++ client/js/pixel-quest.js
@@ -29,11 +29,11 @@ window.PixelQuest = (function() {
   }
 
   PixelQuest.prototype.getPlayerId = function() {
-    var id = getIdentifier()
+    var id = window.PixelQuest.Player.getIdentifier()
 
     if (!id) {
-      id = generateIdentifier()
-      setIdentifier(id)
+      id = window.PixelQuest.Player.generateIdentifier()
+      window.PixelQuest.Player.setIdentifier(id)
     }
 
     return id
@@ -85,34 +85,6 @@ window.PixelQuest = (function() {
     window.onbeforeunload = function() {
       self.socket.emit('player#quit', { id: self.getPlayerId() })
     }
-  }
-
-  /////////////
-  // private //
-  /////////////
-
-  var getIdentifier = function() {
-    var cookies = {}
-
-    ;(document.cookie || "").split(';').forEach(function(pair) {
-      var split = pair.split("=")
-      if (split[0]) {
-        cookies[split[0].trim()] = split[1].trim()
-      }
-    })
-
-    return cookies.PixelQuestIdentifier
-  }
-
-  var setIdentifier = function(id) {
-    document.cookie = 'PixelQuestIdentifier=' + id + '; expires=Tue, 1 Jan 2030 00:00:00 UTC; path=/'
-  }
-
-  var generateIdentifier = function() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-    })
   }
 
   return PixelQuest
--- client/js/player.js
+++ client/js/player.js
@@ -2,13 +2,55 @@ window.PixelQuest.Player = (function() {
   "use strict"
 
   var Player = function(id, options) {
-    this.id      = id
-    this.options = options || {}
+    this.id             = id
+    this.isActivePlayer = (id === window.PixelQuest.Player.getIdentifier())
+    this.options        = options || {}
+    this.renderOptions  = {
+      color:          "#F2D5C0",
+      arrowOffset:    0,
+      arrowDirection: "up",
+      width:          30,
+      height:         30
+    }
+  }
+
+  Player.getIdentifier = function() {
+    var cookies = {}
+
+    ;(document.cookie || "").split(';').forEach(function(pair) {
+      var split = pair.split("=")
+      if (split[0]) {
+        cookies[split[0].trim()] = split[1].trim()
+      }
+    })
+
+    return cookies.PixelQuestIdentifier
+  }
+
+  Player.setIdentifier = function(id) {
+    document.cookie = 'PixelQuestIdentifier=' + id + '; expires=Tue, 1 Jan 2030 00:00:00 UTC; path=/'
+  }
+
+  Player.generateIdentifier = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    })
   }
 
   Player.prototype.render = function(ctx) {
-    ctx.fillStyle = "#F2D5C0"
-    ctx.fillRect(this.options.x, this.options.y, 30, 30)
+    ctx.fillStyle = this.renderOptions.color
+
+    ctx.fillRect(
+      this.options.x,
+      this.options.y,
+      this.renderOptions.width,
+      this.renderOptions.height
+    )
+
+    if (this.isActivePlayer) {
+      renderArrow.call(this, ctx)
+    }
   }
 
   Player.prototype.move = function(direction, step) {
@@ -38,6 +80,42 @@ window.PixelQuest.Player = (function() {
 
   Player.prototype.update = function(data) {
     this.options = data
+  }
+
+  // private
+
+  var renderArrow = function(ctx) {
+    var width = 4
+
+    ctx.fillRect(
+      this.options.x + this.renderOptions.width / 2 - width / 2,
+      this.options.y - width * 2 - parseInt(this.renderOptions.arrowOffset),
+      width, width
+    )
+
+    ctx.fillRect(
+      this.options.x + this.renderOptions.width / 2 - width / 2 - width,
+      this.options.y - width * 2 - parseInt(this.renderOptions.arrowOffset) - width,
+      width, width
+    )
+
+    ctx.fillRect(
+      this.options.x + this.renderOptions.width / 2 - width / 2 + width,
+      this.options.y - width * 2 - parseInt(this.renderOptions.arrowOffset) - width,
+      width, width
+    )
+
+    if (this.renderOptions.arrowOffset === 5) {
+      this.renderOptions.arrowDirection = 'down'
+    } else if (this.renderOptions.arrowOffset === 0) {
+      this.renderOptions.arrowDirection = 'up'
+    }
+
+    if (this.renderOptions.arrowDirection === 'up') {
+      this.renderOptions.arrowOffset = this.renderOptions.arrowOffset + 0.25
+    } else {
+      this.renderOptions.arrowOffset = this.renderOptions.arrowOffset - 0.25
+    }
   }
 
   return Player
