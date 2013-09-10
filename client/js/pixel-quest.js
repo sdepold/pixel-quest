5d201564eb9b75b0b25dce54d2097889e5575dda
Sascha Depold <sascha@depold.com>
2013-09-10 22:13:48 +0200

    update x,y and hp

--- client/js/pixel-quest.js
+++ client/js/pixel-quest.js
@@ -58,7 +58,7 @@ window.PixelQuest = (function() {
     this.socket.on('player#update', function(_player) {
       var player = self.game.getObject(_player.id)
 
-      ;(['experience', 'achievements']).forEach(function(field) {
+      ;(['experience', 'achievements', 'x', 'y', 'hp']).forEach(function(field) {
         player.object.options[field] = _player.options[field]
       })
     })
